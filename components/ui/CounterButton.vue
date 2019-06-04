a9c4746bfa70d96ead0a7cb2805a2546a819e218
Satoshi Yoshida <satoshi.yoshida@needswell.com>
2019-06-04 09:29:08 +0900

    bugfix CounterButton.vue

--- components/ui/CounterButton.vue
+++ components/ui/CounterButton.vue
@@ -2,13 +2,13 @@
   <div>
     <div class="text-xs-center">
       <v-btn large color="yellow accent-1" @click="execIncrement">Count Up</v-btn>
-  </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  method:{
+  methods:{
     execIncrement(){
       this.$store.commit('increment')
     }
