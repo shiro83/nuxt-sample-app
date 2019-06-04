6fd16be9f1c2e8be44dfd5651d0ba4daf489c6b1
Satoshi Yoshida <satoshi.yoshida@needswell.com>
2019-06-04 11:35:25 +0900

    Learned tutorial 4

--- pages/todo.vue
+++ pages/todo.vue
@@ -3,6 +3,15 @@
     <p class="text-xs-center display-3 font-weight-medium">
       Todo
     </p>
+    <v-form>
+      <v-container>
+        <v-layout row wrap>
+          <v-flex xs12 sm6 offset-sm3>
+            <v-text-field input label="search" outline name="findContent" v-model="findContent" />
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-form>
 
     <v-layout row>
       <v-flex xs12 sm6 offset-sm3>
@@ -10,6 +19,18 @@
           <v-subheader>
             Your Life
           </v-subheader>
+          
+          <v-container>
+            <v-layout row wrap>
+              <v-flex sm10>
+              <v-text-field label="todo?" outline name="inputContent" v-model="inputContent" />
+              </v-flex>
+              <v-flex>
+                <v-btn @click="addTodo()">Add</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+
           <v-list two-line>
             <div v-for="(todoData,index) in todoDatas">
              <v-divider></v-divider>
@@ -26,6 +47,7 @@
                   <v-list-tile-sub-title>
                     {{todoData.created}}
                   </v-list-tile-sub-title>
+                  <button @click="doneTodo(todoData)">Done</button>
                 </v-list-tile-content>
               </v-list-tile>
             </div>
@@ -38,9 +60,24 @@
 
 <script>
 export default {
+  data(){
+    return{
+      inputContent:'',
+      findContent:'',
+    }
+  },
  computed: {
     todoDatas () {
-      return this.$store.state.todo.list
+      return this.$store.getters['todo/filterList'](this.findContent);
+    }
+  },
+  methods:{
+    addTodo(){
+      this.$store.commit('todo/add',this.inputContent);
+      this.inputContent='';
+    },
+    doneTodo(todo){
+      this.$store.commit('todo/remove',todo);
     }
   }
 }
--- store/todo.js
+++ store/todo.js
@@ -4,3 +4,37 @@ export const state = () => ({
       {content: 'fugafuga', created: '2019-03-31 16:30'}
   ]
 })
+
+export const getters={
+  filterList:(state) => (text) =>{
+    if (!text) {return state.list}
+    const todos =state.list.filter(function(x,index){
+      if((x.content).indexOf(text)>=0) return true;
+    })
+    return todos
+    // こういう書き方もできる
+    // return state.list.filter(x => (x.content).indexOf(text) >= 0))
+  }
+}
+
+export const mutations={
+  add (state,text){
+    var d =new Date()
+    var fmt =d.getFullYear()
+          +'-'+('00' + (d.getMonth()+1)).slice(-2)
+          +'-'+('00' + d.getDate()).slice(-2)
+          +' '+('00' + d.getHours()).slice(-2)
+          +':'+('00' + d.getMinutes()).slice(-2)
+    state.list.push({
+      content:text,
+      created:fmt,
+      done:false,
+    })
+  },
+  remove(state,todo){
+    // 合致した配列を詰めつつ削除
+    // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
+    // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+    state.list.splice(state.list.indexOf(todo),1)
+  }
+}
